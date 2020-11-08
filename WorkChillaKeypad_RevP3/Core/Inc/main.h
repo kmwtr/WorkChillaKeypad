1c82f3052e6372e0caf8488f923a0f735816a044
kmwtr <0x550x45@gmail.com>
2020-11-08 20:38:48 +0900

    renamed GPIO

--- WorkChillaKeypad_RevP3/Core/Inc/main.h
+++ WorkChillaKeypad_RevP3/Core/Inc/main.h
@@ -58,44 +58,44 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define OUT0_Pin GPIO_PIN_0
-#define OUT0_GPIO_Port GPIOA
-#define OUT1_Pin GPIO_PIN_1
-#define OUT1_GPIO_Port GPIOA
-#define OUT2_Pin GPIO_PIN_2
-#define OUT2_GPIO_Port GPIOA
-#define OUT3_Pin GPIO_PIN_3
-#define OUT3_GPIO_Port GPIOA
-#define OUT4_Pin GPIO_PIN_4
-#define OUT4_GPIO_Port GPIOA
-#define OUT5_Pin GPIO_PIN_5
-#define OUT5_GPIO_Port GPIOA
-#define OUT6_Pin GPIO_PIN_6
-#define OUT6_GPIO_Port GPIOA
-#define OUT7_Pin GPIO_PIN_7
-#define OUT7_GPIO_Port GPIOA
+#define OUT_0_Pin GPIO_PIN_0
+#define OUT_0_GPIO_Port GPIOA
+#define OUT_1_Pin GPIO_PIN_1
+#define OUT_1_GPIO_Port GPIOA
+#define OUT_2_Pin GPIO_PIN_2
+#define OUT_2_GPIO_Port GPIOA
+#define OUT_3_Pin GPIO_PIN_3
+#define OUT_3_GPIO_Port GPIOA
+#define OUT_4_Pin GPIO_PIN_4
+#define OUT_4_GPIO_Port GPIOA
+#define OUT_5_Pin GPIO_PIN_5
+#define OUT_5_GPIO_Port GPIOA
+#define OUT_6_Pin GPIO_PIN_6
+#define OUT_6_GPIO_Port GPIOA
+#define OUT_7_Pin GPIO_PIN_7
+#define OUT_7_GPIO_Port GPIOA
 #define SWICH_Pin GPIO_PIN_0
 #define SWICH_GPIO_Port GPIOB
-#define IN0_Pin GPIO_PIN_1
-#define IN0_GPIO_Port GPIOB
-#define OUT8_Pin GPIO_PIN_8
-#define OUT8_GPIO_Port GPIOA
-#define OUT9_Pin GPIO_PIN_9
-#define OUT9_GPIO_Port GPIOA
+#define IN_0_Pin GPIO_PIN_1
+#define IN_0_GPIO_Port GPIOB
+#define OUT_8_Pin GPIO_PIN_8
+#define OUT_8_GPIO_Port GPIOA
+#define OUT_9_Pin GPIO_PIN_9
+#define OUT_9_GPIO_Port GPIOA
 #define LED_Pin GPIO_PIN_10
 #define LED_GPIO_Port GPIOA
 #define TCM809_Pin GPIO_PIN_15
 #define TCM809_GPIO_Port GPIOA
-#define IN1_Pin GPIO_PIN_3
-#define IN1_GPIO_Port GPIOB
-#define IN2_Pin GPIO_PIN_4
-#define IN2_GPIO_Port GPIOB
-#define IN3_Pin GPIO_PIN_5
-#define IN3_GPIO_Port GPIOB
-#define IN4_Pin GPIO_PIN_6
-#define IN4_GPIO_Port GPIOB
-#define IN5_Pin GPIO_PIN_7
-#define IN5_GPIO_Port GPIOB
+#define IN_1_Pin GPIO_PIN_3
+#define IN_1_GPIO_Port GPIOB
+#define IN_2_Pin GPIO_PIN_4
+#define IN_2_GPIO_Port GPIOB
+#define IN_3_Pin GPIO_PIN_5
+#define IN_3_GPIO_Port GPIOB
+#define IN_4_Pin GPIO_PIN_6
+#define IN_4_GPIO_Port GPIOB
+#define IN_5_Pin GPIO_PIN_7
+#define IN_5_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
